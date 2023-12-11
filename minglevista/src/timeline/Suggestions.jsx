54f7cf618b072d01647e54ba8b646cdd03764208
Abbe <abdullah.abou.shihab@icloud.com>
2023-12-11 11:08:44 +0100

    new style

--- minglevista/src/timeline/Suggestions.jsx
+++ minglevista/src/timeline/Suggestions.jsx
@@ -1,10 +1,72 @@
 import React from 'react'
 import './Suggestions.css'
+import { Avatar } from "@mui/material";
 
 function Suggestions() {
   return (
-    <div>
-      Suggestions
+    <div className='suggestions'>
+
+
+      <div className='suggestions__title'>Suggestions for you</div>
+
+
+      <div className='suggesstions__usernames'>
+
+      <div className='suggesstion__username'>
+      <div className='username__left'>
+        <span className='avatar'>
+          <Avatar>A</Avatar>
+        </span>
+        <div className='username__info'>
+          <span className='username'>Abbelito_</span>
+          <span className='relation'>New to instagram</span>
+        </div>
+      </div>
+      <button className='follow__button'>Follow</button>
+      </div>
+
+      <div className='suggesstion__username'>
+      <div className='username__left'>
+        <span className='avatar'>
+          <Avatar>SA</Avatar>
+        </span>
+        <div className='username__info'>
+          <span className='username'>SaraAli_</span>
+          <span className='relation'>New to instagram</span>
+        </div>
+      </div>
+      <button className='follow__button'>Follow</button>
+      </div>
+
+      <div className='suggesstion__username'>
+      <div className='username__left'>
+        <span className='avatar'>
+          <Avatar>M</Avatar>
+        </span>
+        <div className='username__info'>
+          <span className='username'>Moomo00</span>
+          <span className='relation'>New to instagram</span>
+        </div>
+      </div>
+      <button className='follow__button'>Follow</button>
+      </div>
+
+      <div className='suggesstion__username'>
+      <div className='username__left'>
+        <span className='avatar'>
+          <Avatar>A</Avatar>
+        </span>
+        <div className='username__info'>
+          <span className='username'>Alawie_</span>
+          <span className='relation'>New to instagram</span>
+        </div>
+      </div>
+      <button className='follow__button'>Follow</button>
+      </div>
+
+      </div>
+
+
     </div>
   )
 }
--- minglevista/src/timeline/Timeline.jsx
+++ minglevista/src/timeline/Timeline.jsx
@@ -1,17 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Timeline.css";
 import Suggestions from "./Suggestions";
-import Posts from "./posts/Posts";
+import Post from "./posts/Posts";
 
 function Timeline() {
+  const [posts, setPosts] = useState([
+    {
+      user: "Abbelito_",
+      postImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3HCJlI_HQvwiok906NGGbyLwFCKes2Dx21Q&usqp=CAU",
+      likes: 45,
+      timestamp: "6d"
+    },
+    {
+      user: "Alawie_",
+      postImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuGAmXeL9WiSTDKdhUkpVi9CXueDLI-TEzjA&usqp=CAU",
+      likes: 322,
+      timestamp: "12h"
+    },
+    {
+      user: "Reemoz00",
+      postImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRadPwZgah-uHLeV0hRbLbc8NbChYjIRHeg5w&usqp=CAU",
+      likes: 123,
+      timestamp: "2d"
+    },
+    {
+      user: "Sara58",
+      postImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0btn-EDqmQJLPhjyza9zxCzTvZSmSqXqlEQ&usqp=CAU",
+      likes:543,
+      timestamp: "3h"
+    }
+  ]);
+
   return (
     <div className="timeline">
       <div className="timeline__left">
-        <div className="timeline__post">
-          <Posts />
-          <Posts />
-          <Posts />
-          <Posts />
+        <div className="timeline__posts">
+        {posts.map((post) => (
+            <Post
+              user={post.user}
+              postImage={post.postImage}
+              likes={post.likes}
+              timestamp={post.timestamp}
+            />
+          ))}
         </div>
       </div>
       <div className="timeline__right">
